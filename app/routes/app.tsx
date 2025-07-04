ebe6c60d62251862da43c53a46cc4b475c6949fc
Aayush Paliwal <aayushpaliwal653@gmail.com>
2025-07-05 00:32:33 +0530

    Configures the Shopify app settings
    Updates the Shopify app configuration file with client ID, name, handle, application URL, and embedded app status.
    
    Configures webhooks for app uninstallation and scopes update events.
    
    Defines access scopes for product writing.
    
    Sets up authentication redirect URLs.
    
    Disables embedded POS.

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -19,6 +19,7 @@ export default function App() {
   const { apiKey } = useLoaderData<typeof loader>();
 
   return (
+    
     <AppProvider isEmbeddedApp apiKey={apiKey}>
       <NavMenu>
         <Link to="/app" rel="home">
